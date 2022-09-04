e0e6bc7d529de6534ededa8ae5a17e9a7529964a
cayuan <cayuan@gmail.com>
2022-09-04 20:03:24 +0800

    20220904

--- CAA/Form1.Designer.vb
+++ CAA/Form1.Designer.vb
@@ -22,10 +22,264 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox13 = New System.Windows.Forms.GroupBox()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label64 = New System.Windows.Forms.Label()
+        Me.lblVersion = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.picStamp = New System.Windows.Forms.PictureBox()
+        Me.lblCopyright = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.cmdExit = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AsdToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox13.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.picStamp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox13
+        '
+        Me.GroupBox13.Controls.Add(Me.Label22)
+        Me.GroupBox13.Controls.Add(Me.Label64)
+        Me.GroupBox13.Controls.Add(Me.lblVersion)
+        Me.GroupBox13.Location = New System.Drawing.Point(199, 35)
+        Me.GroupBox13.Name = "GroupBox13"
+        Me.GroupBox13.Size = New System.Drawing.Size(250, 61)
+        Me.GroupBox13.TabIndex = 32
+        Me.GroupBox13.TabStop = False
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label22.Location = New System.Drawing.Point(5, 10)
+        Me.Label22.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(243, 24)
+        Me.Label22.TabIndex = 25
+        Me.Label22.Text = "CAY's MAchine Learning"
+        '
+        'Label64
+        '
+        Me.Label64.AutoSize = True
+        Me.Label64.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label64.Location = New System.Drawing.Point(40, 34)
+        Me.Label64.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label64.Name = "Label64"
+        Me.Label64.Size = New System.Drawing.Size(67, 18)
+        Me.Label64.TabIndex = 30
+        Me.Label64.Text = "CAYMAL"
+        '
+        'lblVersion
+        '
+        Me.lblVersion.AutoSize = True
+        Me.lblVersion.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblVersion.Location = New System.Drawing.Point(111, 33)
+        Me.lblVersion.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(38, 20)
+        Me.lblVersion.TabIndex = 26
+        Me.lblVersion.Text = "v0.1"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.picStamp)
+        Me.GroupBox3.Controls.Add(Me.lblCopyright)
+        Me.GroupBox3.Controls.Add(Me.PictureBox1)
+        Me.GroupBox3.Controls.Add(Me.LinkLabel2)
+        Me.GroupBox3.Controls.Add(Me.Label13)
+        Me.GroupBox3.Controls.Add(Me.Label14)
+        Me.GroupBox3.Controls.Add(Me.LinkLabel1)
+        Me.GroupBox3.Location = New System.Drawing.Point(23, 312)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(335, 107)
+        Me.GroupBox3.TabIndex = 33
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Author Contact Infomation"
+        '
+        'picStamp
+        '
+        Me.picStamp.Image = Global.CAA.My.Resources.Resources.CA_Orange
+        Me.picStamp.Location = New System.Drawing.Point(280, 11)
+        Me.picStamp.Name = "picStamp"
+        Me.picStamp.Size = New System.Drawing.Size(50, 50)
+        Me.picStamp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picStamp.TabIndex = 32
+        Me.picStamp.TabStop = False
+        '
+        'lblCopyright
+        '
+        Me.lblCopyright.AutoSize = True
+        Me.lblCopyright.Font = New System.Drawing.Font("Arial Narrow", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCopyright.Location = New System.Drawing.Point(9, 86)
+        Me.lblCopyright.Name = "lblCopyright"
+        Me.lblCopyright.Size = New System.Drawing.Size(267, 15)
+        Me.lblCopyright.TabIndex = 23
+        Me.lblCopyright.Text = "Copyright © 2020,2021 Dr. Cadmus Yuan, All rights reserved. "
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.CAA.My.Resources.Resources.fcu
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 18)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(149, 67)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 18
+        Me.PictureBox1.TabStop = False
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(174, 50)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(113, 13)
+        Me.LinkLabel2.TabIndex = 22
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "www.mcae.fcu.edu.tw"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(174, 24)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(90, 13)
+        Me.Label13.TabIndex = 19
+        Me.Label13.Text = "Dr. Cadmus Yuan"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(174, 37)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(37, 13)
+        Me.Label14.TabIndex = 21
+        Me.Label14.Text = "MCAE"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(174, 66)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(103, 13)
+        Me.LinkLabel1.TabIndex = 20
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "cayuan@fcu.edu.tw"
+        '
+        'cmdExit
+        '
+        Me.cmdExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdExit.Image = Global.CAA.My.Resources.Resources.go_out
+        Me.cmdExit.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.cmdExit.Location = New System.Drawing.Point(666, 349)
+        Me.cmdExit.Name = "cmdExit"
+        Me.cmdExit.Size = New System.Drawing.Size(66, 62)
+        Me.cmdExit.TabIndex = 34
+        Me.cmdExit.Text = "Exit"
+        Me.cmdExit.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.cmdExit.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 35
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AsdToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'AsdToolStripMenuItem
+        '
+        Me.AsdToolStripMenuItem.Name = "AsdToolStripMenuItem"
+        Me.AsdToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.AsdToolStripMenuItem.Text = "Open"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(100, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmdExit)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox13)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox13.ResumeLayout(False)
+        Me.GroupBox13.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.picStamp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GroupBox13 As GroupBox
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label64 As Label
+    Friend WithEvents lblVersion As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents picStamp As PictureBox
+    Friend WithEvents lblCopyright As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents cmdExit As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AsdToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
 End Class
--- CAA/Form1.vb
+++ CAA/Form1.vb
@@ -1,3 +1,172 @@
-﻿Public Class Form1
+﻿
+
+''+++++++++++++++++++++++++oosyhdddddddddddmmmmmmmmNNNNNNNmdmmNNNNNNNNNNNNmddhyhddhdmmmmmmmmmmmmmdddhh
+''+++++++++++++++++++++osyhhddddddddmmmmmmmmmmmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNmmddhddmmNNNNNNNNNNNNNNmmm
+''++++++++++++++++++oyhddddddddddmmmmmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmdddmmmNNNNNNNNNNNNNNNN
+''+++++++++++++++ossyddddhhhdddddmmmmmmmmmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNMMMMNNNNNmdmmmmmNNNNNNNNNNNNNNN
+''++++++++++++ooosyhhhdddhhhhddddddmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNMMNNNmmmmmNNNNNNNNNNNNNNN
+''o+ooooooooooosyhhhhdddddmmmmddmmmmmmmmNNNNNNNNNNNNNNNmmmmmmNNNNNNNNNNNNNMNNNMMMMNNmmmNNNNNNNNNNNNNNN
+''++++++++++ooshhhhddddddhdddddddmmmmmmmNNNNNNNNNNNNNNmmmmmmmmmmNNNNNNNNNNNNNMMMNMNNNNmmmmmmmNmNNNNNNN
+''+++++++++++shhdddddddddddmmmddmmmmmNNNNNNNNNNNNNNNmmmmmmmmmmmmNNNNNNNNNNNNNNNNNMNNNNmdhhhhhhhhhhhhhh
+''/////////+sdddddddddddddddddmmmmmmmNNNNNNNNNNNNNNmmddddmmmmmmmmNNNNNNNNNNNNMNNNNNNNNmdhsoooooooooooo
+''/////////odmmdddddddddddmmmmmmmmmmmmNNNNNNNNNNNNNmdhhddmmmmmmmNNNNNNNNNNNNNNMNNNNNNNNhyy+///////////
+'':::::://+dmmNmmddddmdmmmmmmmmmNNNNNmmmmNNNNmmmmmmmmdddmmmmmmNNNNNNNNNNNNNNNNNNNNNNNNNdsy+::::://////
+''//:::::/ymmmmmmddddmmmmdmmmmmmNNNNNNNmmmmmdhdhyyhddhhddmmmmmmmNNNNNNNNNNNNNNNNNNNNNNNNho+///////////
+''/////:/+mmmdddddddmmdmddmmmmmmNNNNNNNmmdddhyhyssssyyyhhddddmmmmmmNNNNNNNNNNNNNNNNNNNNNm++///////////
+''++/////ymdddddddddddmdddmmmmmmmNNmmmmmdhysooso++/++oosssyhhhddmmmmmmNNNNNNNNNNNNNNNmNNNs////////////
+''+++///odmddddddmdddddddmmmmmmmmmmmmdhyso+++/////////+osossyyhhhdmmmmmNNNNNNNNNNNNNNNNmNs////////////
+''oo+++sddddddddddddddhhddddddmdddhyso+//:::::::::::/+++oooossyhhhdddmmmNNNNNNNNNNNNNNNNdo////////////
+''soo+odmmmmmmmmmmmddhyyyhhhhhhyso+//::::------:::::/+++oooosssshhhdddmmmNNNNNNNNNNNNNNmd+////////////
+''yssosdNmmmmmmmmmmdhyssysssso+///:::::-------:::::://++ooooosssyhdhdddmmmNNNNNNNNNNNNNNms+///////////
+''yyssydNNNmmmmmmmmdhyysooo/////:::----------:::://:/++++oooooossyhddddmmmmmNNNNNNNNNNNNms////////////
+''hyysohmmmmmmmmmmdddho++/::::::::-----------::::::://+++++oooossyyhddddmmmmmNNNNNNNNNNNh+////////////
+''hhysohmNNmddmmmddddyoo::--:::::------------:::::::////++++ooossyyyhdddmmmmNNNNNNNNNNNNd/////////////
+''dhysohmmmdmmmmmmmddys/:-----------------:---::::::://+++++ooossyyyhddddmmmNNNNNNNNNNNNh+/::://://::/
+''dhyssohmmmmNNmmdmdhs+:-----------------:---------::/+++++++oossyyhhhddddmmNNNNNNNNNNNmd/::::::::::::
+''dhysooyddmNNNNmddhyo/:--------...-.-----------.---://++++++ooossyhhhhhhddmmNNNNNNNNNNhs/::::::::::::
+''dhyso+oyyhdmNNmmdhs//-:-.----.......---:------..--::///++ossssssyyyhhhhhhdmNNNNNNmmmdhys/:::::::::::
+''dhyso++o++oshdmmmho/::---::://////:::::::----:--::/+ossyyyyyyyyyyyyyyyyhhhmNmmNmdddhhyyyo::::::::---
+''hhyso+++::///:/ymd+::-::////++oo+oo+++++/:--:////ossssssoosssyyhhhhyyyyhhdddmNNdddhhhyyys::::::-----
+''yysso++/-+o+/:-/dh+::-::------:://++++++/:-:/+o++++////++oossyyyyhhhyyyyyhhdNNNmdhhhyyyys:::::::::--
+''ssoo++++:/+/:--:ss::-::///::://+oossyso+/:--/+oo++oo+++++osssyyhhhddhyyhyyyhmNNmdhhhyyys+:::::::::::
+''oo+++++++:/+/:-/y+//++:--:::/++oyyhyysoss+/+oyyysooossosyyyhhhhhyyymNmhyssshdmmmdhhhhyso/:::::::::::
+''+++++++ooo::+/:+yoyhyo-.--::/+++osssoo/:shssyhdyysoo++oosssyyyyyyyydhyyssssyhhdmdhhhyy+:::::::::::::
+''oooooooooss/:///o/-:s/-..--:://++ooo+/::oo/:/ohhyso+++++ooossssssssyyysssssyyydmdhyys/::::::::::::::
+''oooooooooooo/-::::---+/-----:://///:::://:--:+syyyso+++++++++++oosysssssssyysydhhhys/:::::::::::::::
+''osssssoo+++++/-.-..--.-:::::://///////:---.-:+osssyssoo+++++++++ossssysyyyyysyhyhyy+::::::::::::::::
+''oossysoo++///+:.-..--....-----::::::---....-:/+ssssso+///:::::/+oosssyyyyyyssyhhyys:::::::::::::::::
+''oossyyso++///++.....---.......----:::--....-:/+osyyys+//::::://++ossyyyyyyysshyyys+:::::::::::::::::
+''oosssysso+++++o+-..-----.....---:::::-..`...-:+oossyyso//:::://++ossyyyyyyyys+ooo+::::::::::::::::::
+''ooossssssooooooss+++/----..---::::://-.....-:/+oosssyhs+/::////+ossyyyyyyyyyo:::::::::::::::::::::::
+''oooosssssssooosssyyys:------::::::::--://:::/+osyssyyyo+//////++osyyyyyyyyyyo////:::::::://:::::::::
+''+ooooosssssooooossyyy+-----:::--::-...---://+/++oosssoooo+///++ossyyyyyyyyyy+///////::://///////:::/
+''++++ooossssoooooossyys/----::-----.....---:/+//++oosssooooo+++oossyyyyyyyyys/////////:://///////////
+''////++ooosssooooosssyys/---------....-----:/+//+oooosssssssso+oossyyyyyyhhyo////////////////////////
+''::///++oosssssssssssssss:--------.--------::///oooossssssssys+oossyyyyyyhhyo/////////++++++++++++++/
+''::://++ossyyyyyssoooooooo:------------:///++++ssssyyyyysyyyysooossyyyyyhhyhs+/++++++++++++++++++++++
+''////++oosssssyysooo+++++++:------:://+ooo+///oooosyyyhhsssssoooosyyyyyhhhhdy+:+ooooooooooooooooooooo
+''//+++oossssssssooo+++++++++:-----:/:::/sss//://+ossyyyyssssoooosyyyyyhhhhhmy/:/hsooooooooooooooooooo
+''+++++oooooooooooo+++++++++++/---::::--:::+ooosoooosssyyyssssoosyyyyhhhhhydd+:::yhyysoooooooooooooooo
+''+++++++++++++++++++++++++++++---::::::::::::/:/+++syhhysssssssyyyyhhhhhhhs/::::ydhhhhsoooooooooooooo
+''+++++++//+++++++++++++++++++-`-::-----::////+++ossyyyssssosssyyyhhhhhhhy+::::::ymddyoddyoooooooooooo
+''oo++++//////+++++++++++++++/`.:+:--------:://+++oooooooooossyyyyyyhhyyo:::::::+dmmmmshmmdhso++++++oo
+''sooo++++////+++++++++++++oo: -+o/---------:://+++++++ooooosyyyyyyyyys/:--::::/hmmmmmymmmmmmdyo+++ooo
+''ssooo+++++++++++oossyhhhhhy: -oys-.--::::://++++ooo+++oossyyyyyyys+:----:::::ymmmmNNmmmmmmmmmmdyoooo
+''sssoooooossyyhdddmdddhhhhyy: `/hh:..--:://++++ooooo++oooosssssso/-------:--:ymddmNNmmmmmmmmmmNmmmdhs
+''hdddddddddddmmmddhhyyyyyyyy/  `sdo-..---:://++++++++++oooooo++:-...-------:ymdyymmmmmmmmmmmNNNNNNNNm
+''dddhhhhhhhddddhyyyyyyyyyyyh+   `oy+:------::////+////+++//:-..`...-------:ymdhhdmmmmmmmmmmmNNNNNNNNN
+''hhhhhyyhhddddhyyyyyyyyyyyhh/    `/yo/:-------:::::://::-.``````...----.-:ydmhhdmmmmmmmmmmmmmmmNNNNNN
+''hhhyhyyyhdddhyyyyyyyyyyyydo/`     .os+/:------------..`````````.......--sddddmmmmmmmmmmmmmmmmmmmmmmm
+''hyyyhyyydddhyyyyyyyyyyyyhy+/`      `-oo+/::------.`` ```````````......-odyhddmmmmmmmmmmmmmmmmddddmdd
+''yyhhhyyhhdhyyyyyyyyyyyyyho:+`        `-+o+//:-.`     ```````````.....-oddhddddddddmmmmmmmmmmmddmmmmd
+''yhhhyyyhhhhyyyyyyyyyyyyhy:-+-           .:+/-`      ```` ``````......+hdhdddddddddmmmmmmmmmddddmmmdd
+''hhhhyyyhhhyyyyyssyyyyyyho::+/        `./+:.              ``````...../hhhdddddddddddmmmmmmddddddddddd
+''hhhyyyyyhyyyyyyyyyyyyyys+-:o+      `+syyyhh/`          `````.```.`.:yhhhhddddddddddddmmmdddddddddddd
+''hhyyyyyyyyyyyyyyyyyyyyyoo//o/     .yyyhhhhhhs.        ``````.`..`.:yhhhhhhdhdddhddddddddddddddddddhh
+
+
+
+''`````````````````````````````````````````````````````````````````````.:/:.``````````````````````````
+''````````````````````````````````````````````````````````````.````.://-..````````````````````````````
+''```````````````````......``....................................:::...```````````````````````````````
+''````````````.`..............................................-::-............``..````````````````````
+''``````````................................................-/:-...........................```````````
+''```````................................................/o//-...............................```.`````
+''````...............................................:s-/s/-......................................````
+''..................................................:ho+/.............................................
+''..........................................-.--:s--sy+:-.............................................
+''.....................................-....----yo-+/------...........................................
+''..............................--.------------oo/o:-------...........................................
+''..............................---------------oy+------------..--....................................
+''..........................-------------------/:----------------------------------..--...............
+''......................-------------------ss-:+---------------------------------------------.........
+''...................---------------:y----:dy-+:-----------------------------------------------.......
+''...............-..---------------/sh:---/do:+--------------------------------------------------.....
+''..............------------:yy:--/s:+o---/m///----------------------------------------------------..-
+''............--------------o/+s:/s:-:y---/h:+:-------------------------------------------------------
+''............-------------o+--+ys:---y:-:yy/+--------------------------------------------------------
+''...........-------------/+---/do----+s/soy+:-:------------------------------------------------------
+''.........--------------:s:--/s/y/---:+o/:ss:-------::-----------------------------------------------
+''.......--------://:----o/--/s:-/y:-::--::sy:---::::::-----------------------------------------------
+''......--------o+:/s---:y--/s:---oo::--:::+s:::-:::::::-::----::-------------------------------------
+''......-------+o---+o:-o+-:y/----/y:::::::/s::::::::::::::::::::::::::::::::::::::-:::---------------
+''...----------y:----:-:y:--:++///os:::::::/h::::::::::::::::::::::::::::::::::::::::-:::-------------
+''...---------:y-------/s-----::///:-::::::/d::::::::::::::::::::::::::::::::::::::::::-:-------------
+''.-----------:y-------s/----::-:::::::::::/h+::::::::::::::::::::::::::::::::::::::::::::------------
+''------------/y------/s:--::::::-:::::::::/ss::::::::::::::::::::::::::::::::::::::::::::------------
+''------------:y:----:s+-:-:::::::::::::::::ss:::::::::::::::::::::::::::::::::::::::::::::::---------
+''------------:y/:--:+s:::::::::::::::::::::sy/:::::::::::::::::::::::::::::::::::::::::::::::--------
+''-------------+s:::/y:--:::::::::::::::::::oh+:::::::::::::::::::::::::::::::::::::::::::::----------
+''-------------:oo++s/--::::::::::::::::::::/hs::::::::::::::::::::::::::::::::::::::::::::::---------
+''---------------://:::-:::::::::::::::::::::sd::::::::::::::::::::::::::::::::::::::::::::-:---------
+''-----------:-:-::::::::::::::::::::::::::::/o::::::::::::::::::::::::::::::::::::::::::::::---------
+''------:---:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::-----------
+''---------::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::-------------
+''-:----::::::::--::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::----------------
+'':::::::::::::-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::---------------
+
+
+
+
+' The ascii art
+'  pure text: https://patorjk.com/software/taag/#p=testall&f=Graffiti&t=test
+'  draw by yourself: https://kirilllive.github.io/ASCII_Art_Paint/ascii_paint.html
+'
+'
+'
+'
+Imports System.Threading
+Imports System.IO
+
+'Imports MathNet.Numerics.LinearAlgebra.Double
+
+Imports CAA
+
+
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim x As Integer = CAA.CAA_const_structure.KeyWindowStartPosition.form1_x
+
+        Dim y As Integer = CAA.CAA_const_structure.KeyWindowStartPosition.form1_y
+        Me.Location = New Point(x, y)
+        lblCopyright.Text = CAA.CAA_const_structure.CAA_copyright.copywright
+
+
+
+    End Sub
+
+
+
+#Region "UI"
+    Private Sub cmdExit_Click(sender As Object, e As EventArgs) Handles cmdExit.Click
+        Call Unloadme()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Call Unloadme()
+    End Sub
+
+#End Region
+
+
+#Region "Form "
+
+
+
+    Private Sub UnloadMe()
+        Me.Close()
+    End Sub
+
+
+
+
+
+#End Region
+
+
+
+
 
 End Class
